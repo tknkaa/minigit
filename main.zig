7bc76d45f88c240ad983d6e3bb52cafa476aa85f
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2026-02-05 18:44:35 +0800

    feat: add array demonstrations and basic control flow tests

--- main.zig
+++ main.zig
@@ -1,5 +1,11 @@
 const std = @import("std");
 
 pub fn main() void {
-    std.debug.print("Hello, {s}!\n", .{"World"});
+    const a = [5]u8{ 'h', 'e', 'l', 'l', 'o' };
+    const b = [_]u8{ 'w', 'o', 'r', 'l', 'd' };
+    std.debug.print("{any}", .{a});
+    std.debug.print("{any}", .{b});
+    const length = b.len;
+    std.debug.print("{}", .{length});
 }
+
--- test.zig
+++ test.zig
@@ -5,6 +5,17 @@ test "always succeeds" {
     try expect(true);
 }
 
-test "always fails" {
-    try expect(false);
+test "if statement" {
+    const a = true;
+    var x: u16 = 0;
+    x += if (a) 1 else 2;
+    try expect(x == 1);
+}
+
+test "while" {
+    var i: u8 = 2;
+    while (i < 100) {
+        i *= 2;
+    }
+    try expect(i == 128);
 }
