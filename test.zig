d32e34c5d928f8944f871bc8f3c8f78abe455977
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2026-02-06 15:31:55 +0800

    test: refactor test.zig to focus on memory allocation and pointers

--- test.zig
+++ test.zig
@@ -1,173 +1,36 @@
 const std = @import("std");
 const expect = std.testing.expect;
 
-test "always succeeds" {
-    try expect(true);
+test "allocation" {
+    const allocator = std.heap.page_allocator;
+    const memory = try allocator.alloc(u8, 100);
+    defer allocator.free(memory);
+
+    try expect(memory.len == 100);
+    try expect(@TypeOf(memory) == []u8);
 }
 
-test "if statement" {
-    const a = true;
-    var x: u16 = 0;
-    x += if (a) 1 else 2;
-    try expect(x == 1);
+test "arena allocator" {
+    var arena: std.heap.ArenaAllocator = .init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    _ = try allocator.alloc(u8, 1);
+    _ = try allocator.alloc(u8, 10);
+    _ = try allocator.alloc(u8, 100);
 }
 
-test "while" {
-    var i: u8 = 2;
-    while (i < 100) {
-        i *= 2;
-    }
-    try expect(i == 128);
+fn increment(num: *u8) void {
+    num.* += 1;
 }
 
-test "while with continue expression" {
-    var sum: u8 = 0;
-    var i: u8 = 1;
-    while (i <= 10) : (i += 1) {
-        sum += i;
-    }
-    try expect(sum == 55);
+test "pointers" {
+    var x: u8 = 1;
+    increment(&x);
+    try expect(x == 2);
 }
 
-test "while with continue" {
-    var sum: u8 = 0;
-    var i: u8 = 0;
-    while (i <= 3) : (i += 1) {
-        if (i == 2) continue;
-        sum += i;
-    }
-    try expect(sum == 4);
-}
 
-test "while with break" {
-    var sum: u8 = 0;
-    var i: u8 = 0;
-    while (i <= 3) : (i += 1) {
-        if (i == 2) break;
-        sum += i;
-    }
-    try expect(sum == 1);
-}
-
-test "for" {
-    const string = [_]u8{ 'a', 'b', 'c' };
-    for (string, 0..) |character, index| {
-        _ = character;
-        _ = index;
-    }
-
-    for (string) |character| {
-        _ = character;
-    }
-    for (string, 0..) |_, index| {
-        _ = index;
-    }
-}
-fn addFive(x: u32) u32 {
-    return x + 5;
-}
-
-test "function" {
-    const y = addFive(0);
-    try expect(@TypeOf(y) == u32);
-    try expect(y == 5);
-}
-
-fn fibonacci(n: u16) u16 {
-    if (n == 0 or n == 1) return n;
-    return fibonacci(n - 1) + fibonacci(n - 2);
-}
-
-test "function recursion" {
-    const x = fibonacci(10);
-    try expect(x == 55);
-}
-
-test "defer" {
-    var x: i16 = 5;
-    {
-        defer x += 2;
-        try expect(x == 5);
-    }
-    try expect(x == 7);
-}
-
-test "multi defer" {
-    var x: f32 = 5;
-    {
-        // executed in reverse order
-        defer x += 2;
-        defer x /= 2;
-    }
-    try expect(x == 4.5);
-}
-
-const FileOpenError = error{
-    AccessDenied,
-    OutOfMemory,
-};
-
-const AllocationError = error{OutOfMemory};
-
-test "coerce error from a subset to a superset" {
-    const err: FileOpenError = AllocationError.OutOfMemory;
-    try expect(err == FileOpenError.OutOfMemory);
-}
-
-test "error union" {
-    const maybe_error: AllocationError!u16 = 10;
-    const no_error = maybe_error catch 0;
-    try expect(@TypeOf(no_error) == u16);
-    try expect(no_error == 10);
-}
-
-fn failingFunction() error{Oops}!void {
-    return error.Oops;
-}
-
-test "returning an error" {
-    failingFunction() catch |err| {
-        try expect(err == error.Oops);
-        return;
-    };
-}
-
-fn failFn() error{Oops}!i32 {
-    try failingFunction();
-    return 12;
-}
-
-test "try" {
-    const v = failFn() catch |err| {
-        try expect(err == error.Oops);
-        return;
-    };
-    // never reached
-    try expect(v == 12);
-}
-
-var problems: u32 = 98;
-fn failFnCounter() error{Oops}!void {
-    errdefer problems += 1;
-    try failingFunction();
-}
-
-test "errdefer" {
-    failFnCounter() catch |err| {
-        try expect(err == error.Oops);
-        try expect(problems == 99);
-        return;
-    };
-}
-
-fn createFile() !void {
-    return error.AccessDenied;
-}
-
-test "inferred error set" {
-    const x: error{AccessDenied}!void = createFile();
-    _ = x catch {};
-}
 
 
 
