816b8a097ff9aaf8221d37f9368882ccfa9d89f6
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2026-02-06 23:22:36 +0800

    refactor: simplify main to output hello world to stdout

--- main.zig
+++ main.zig
@@ -1,10 +1,6 @@
 const std = @import("std");
 
-pub fn main() void {
-    const a = [5]u8{ 'h', 'e', 'l', 'l', 'o' };
-    const b = [_]u8{ 'w', 'o', 'r', 'l', 'd' };
-    std.debug.print("{any}", .{a});
-    std.debug.print("{any}", .{b});
-    const length = b.len;
-    std.debug.print("{}", .{length});
+pub fn main() !void {
+    var stdout = std.fs.File.stdout();
+    _ = try stdout.write("Hello, world!\n");
 }
