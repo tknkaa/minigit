a039cc9977e7d6387ff3fe5db6baff7c44a3e923
tknkaa <tknkaa@users.noreply.github.com>
2026-03-22 14:28:04 +0800

    blob and tree

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"bytes"
+	"compress/zlib"
+	"crypto/sha1"
+	"encoding/hex"
 	"fmt"
 	"os"
+	"strconv"
+	"time"
 )
 
 func gitInit() {
@@ -14,11 +20,63 @@ func gitInit() {
 	os.WriteFile(".minigit/HEAD", []byte("ref: refs/heads/main"), 0644)
 }
 
+func gitCommit(message string) {
+	author := "author"
+	date := strconv.Itoa(int(time.Now().Unix()))
+
+	//TODO: go to the same directory as .minigit
+	files, _ := os.ReadDir(".")
+	entries := make([][]byte, 0)
+	for _, file := range files {
+		//TODO: search recursively
+		if !file.IsDir() {
+			filename := file.Name()
+			content, _ := os.ReadFile(filename)
+			// calculate the filename of blob object
+			checksum := sha1.Sum(append([]byte("blob"+" "+strconv.Itoa(len(content))+"\x00"), content...))
+			blobHash := hex.EncodeToString(checksum[:])
+
+			// calculate the content of blob object
+			var b bytes.Buffer
+			w := zlib.NewWriter(&b)
+			w.Write(append([]byte("blob"+" "+strconv.Itoa(len(content))+"\x00"), content...))
+			w.Close()
+			blobContent := b.String()
+			os.WriteFile(".minigit/objects/"+blobHash, []byte(blobContent), 0644)
+
+			mode := "100644"
+			entries = append(entries, append([]byte(mode+" "+filename+"\x00"), checksum[:]...))
+		}
+	}
+
+	// calculate the filename of tree object
+	combined := make([]byte, 0)
+	for _, entry := range entries {
+		combined = append(combined, entry...)
+	}
+	checksum := sha1.Sum(append([]byte("tree"+" "+strconv.Itoa(len(entries))+"\x00"), combined...))
+	treeHash := hex.EncodeToString(checksum[:])
+
+	// calculate the content of tree object
+	var b bytes.Buffer
+	w := zlib.NewWriter(&b)
+	w.Write(append([]byte("tree"+" "+strconv.Itoa(len(entries))+"\x00"), combined...))
+	w.Close()
+	treeContent := b.String()
+	os.WriteFile(".minigit/objects/"+treeHash, []byte(treeContent), 0644)
+}
+
 func main() {
 	command := os.Args[1]
 	switch command {
 	case "init":
 		gitInit()
+	case "commit":
+		if os.Args[2] == "-m" {
+			message := os.Args[3]
+			// git add -A && git commit -m <message>
+			gitCommit(message)
+		}
 	default:
 		panic("no command")
 	}
